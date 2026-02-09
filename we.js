a8b8ed15851990e58a067d750609d22d934dfb89
bradylearnscode <155339586+bradylearnscode@users.noreply.github.com>
2026-02-08 22:44:00 -0700

    Add service worker for caching assets

---
+++ we.js
@@ -0,0 +1,25 @@
+const CACHE = 'pulse-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  './splash.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // network-first for API-like requests (none here) else cache-first
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(()=>caches.match('./'))));
+});
